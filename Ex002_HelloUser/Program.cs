46ab970f7eff6ebfc4d77abca43606265ca00c2c
Alex <252459@gmail.com>
2022-10-26 15:42:07 +0300

    Ex002_HelloUser

--- Ex002_HelloUser/Program.cs
+++ Ex002_HelloUser/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("2Hello, World!");
+﻿Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, " + username);
+
