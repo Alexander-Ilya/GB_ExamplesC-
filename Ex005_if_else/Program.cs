13c53e0b28cf845f2662942be00af51099782423
Alex <252459@gmail.com>
2022-10-27 16:01:59 +0300

    Add .gitignore and Ex005_if-else

--- Ex005_if_else/Program.cs
+++ Ex005_if_else/Program.cs
@@ -1,9 +1,16 @@
-﻿Console.WriteLine("Введите ваше имя: ");
-string username = Console.ReadLine();
-if(username.ToLower() == "маша")
+﻿internal class Program
 {
-Console.WriteLine("Привет, красавица " + username);
-}
-else{
-    Console.WriteLine("Привет, просто " + username);
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите ваше имя: ");
+        string username = Console.ReadLine();
+        if (username.ToLower() == "маша")
+        {
+            Console.WriteLine("Привет, красавица " + username);
+        }
+        else
+        {
+            Console.WriteLine("Привет, просто " + username);
+        }
+    }
 }
